41fa2e4744de64dad709010d432b233de848f89b
Steve Tran <stran@redhat.com>
2022-03-12 18:19:03 -0500

    Add lombok to the project

--- src/main/java/com/steve/service/Person.java
+++ src/main/java/com/steve/service/Person.java
@@ -1,5 +1,14 @@
 package com.steve.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
@@ -7,41 +16,4 @@ public class Person {
     private String city;
     private String state;
     private String country;
-
-    public String getCountry() {
-        return country;
-    }
-    public void setCountry(String country) {
-        this.country = country;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
 }
